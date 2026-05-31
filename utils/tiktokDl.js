946c901e95e1032f2f4651ad8d5294f7d97dfac8
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-05-31 20:23:45 +0700

    Update tiktokDl.js

--- utils/tiktokDl.js
+++ utils/tiktokDl.js
@@ -2,55 +2,28 @@ const axios = require("axios");
 
 async function tiktokDl(url) {
   try {
-    let data = [];
-
-    function formatNumber(integer) {
-      return Number(parseInt(integer))
-        .toLocaleString()
-        .replace(/,/g, ".");
-    }
-
-    function formatDate(n, locale = "id-ID") {
-      let d = new Date(n * 1000);
-      return d.toLocaleDateString(locale, {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      });
-    }
-
     const res = (
       await axios.post(
         "https://www.tikwm.com/api/",
         {},
         {
-          params: {
-            url,
-            count: 12,
-            cursor: 0,
-            web: 1,
-            hd: 1,
-          },
+          params: { url, hd: 1 },
+          timeout: 15000,
           headers: {
             "User-Agent":
               "Mozilla/5.0 (Linux; Android 10) Chrome/116 Mobile Safari/537.36",
-            Referer: "https://www.tikwm.com/",
-          },
-          timeout: 15000,
+            Referer: "https://www.tikwm.com/"
+          }
         }
       )
     ).data.data;
 
-    if (!res) throw new Error("Data kosong");
+    if (!res) throw new Error("No data from API");
+
+    let data = [];
 
     if (res.duration === 0) {
-      res.images?.forEach((v) => {
-        data.push({ type: "photo", url: v });
-      });
+      res.images?.forEach(v => data.push({ type: "photo", url: v }));
     } else {
       data.push(
         { type: "watermark", url: "https://www.tikwm.com" + res.wmplay },
@@ -61,20 +34,25 @@ async function tiktokDl(url) {
 
     return {
       title: res.title,
-      id: res.id,
-      region: res.region,
-      duration: res.duration,
-      taken_at: formatDate(res.create_time),
       cover: "https://www.tikwm.com" + res.cover,
-      data,
-      music_info: {
-        title: res.music_info?.title,
-        author: res.music_info?.author,
-        url: "https://www.tikwm.com" + res.music,
+      duration: res.duration,
+      author: {
+        username: res.author?.unique_id,
+        nickname: res.author?.nickname
       },
       stats: {
-        views: formatNumber(res.play_count),
-        likes: formatNumber(res.digg_count),
+        views: res.play_count,
+        likes: res.digg_count
+      },
+      data
+    };
+
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+module.exports = { tiktokDl };        likes: formatNumber(res.digg_count),
         comment: formatNumber(res.comment_count),
         share: formatNumber(res.share_count),
       },
